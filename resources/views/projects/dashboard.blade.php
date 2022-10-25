9e727afb5716750d920ca396ef4128229d3f5995
ConnaaaR69 <connormattless@gmail.com>
2022-10-25 17:37:30 +0100

    dash init

---
+++ resources/views/projects/dashboard.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <p class="text-5xl mx-auto text-center mt-24">Your Projects</p>
+    <div class="flex justify-center">
+        
+        <div class="flex flex-wrap gap-4 justify-center my-10 w-1/2">
+            
+            @unless(count($projects) == 0)
+    
+            @foreach ($projects as $project)
+            <x-project-card :project="$project" />
+            @endforeach
+
+            @else
+            <p>No Projects Found</p>
+            @endunless
+        </div>
+    </div>
+</x-app-layout>
