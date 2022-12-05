129c5f6a02a36f43f26d003436cdc9097221f6c3
ConnaaaR69 <connormattless@gmail.com>
2022-12-05 10:54:47 +0000

    admin dropdown- alpinejs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,6 +10,7 @@
 
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://unpkg.com/alpinejs@3.2.4/dist/cdn.min.js"></script>
 
     {{-- Bootstrap --}}
     @if (Route::is('admin.project.create'))
@@ -68,13 +69,35 @@
                             <a href="{{ route('companyLead.projects.userprojects') }}" class="navElement"> Company
                                 Dashboard</a>
                         </li>
-                        <li
-                            class="{{ request()->is('admin/company/create') ? 'hidden' : '' }} {{ $user->hasRole('admin') ? '' : 'hidden' }} ">
-                            <a href="{{ route('admin.company.create') }}" class="navElement"> Create a Company</a>
-                        </li>
-                        <li
-                            class="{{ request()->is('admin/company/index') ? 'hidden' : '' }} {{ $user->hasRole('admin') ? '' : 'hidden' }} ">
-                            <a href="{{ route('admin.company.index') }}" class="navElement">Company List</a>
+                        <li>
+                            <div x-data="{ open: false }"
+                                class="{{ $user->hasRole('admin') ? '' : 'hidden' }}  relative inline-block text-left">
+                                <div>
+                                    <button type="button"
+                                        class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 
+                                        id="menu-button"
+                                        aria-expanded="true" aria-haspopup="true" @click="open = !open">
+                                        Admin Options
+                                        <!-- Heroicon name: mini/chevron-down -->
+                                        <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                            fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd"
+                                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </div>
+                                <div x-show="open" @click.outside="open = false"
+                                    class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                                    <div class="py-1" role="none">
+                                        <a href="{{ route('admin.company.create') }}" class="listElement">
+                                            Create a Company</a>
+                                        <a href="{{ route('admin.company.index') }}" class="listElement">Company List</a>
+
+                                    </div>
+                                </div>
+                            </div>
                         </li>
                         <li>
                             <a href="{{ route('admin.projects.logout') }}" class=" btn-primary"
